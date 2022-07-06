8a3ddccffb9b7e43b3f91fbe860e5d3e56733977
selina8918 <68890085+selina8918@users.noreply.github.com>
2022-07-06 19:57:02 +0530

    Add files via upload

---
+++ sms/StudentManagementsystem/src/main/java/com/students/StudentManagementsystemApplication.java
@@ -0,0 +1,18 @@
+package com.students;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+//@ComponentScan(basePackages = {" com.students.controller"})
+
+public class StudentManagementsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementsystemApplication.class, args);
+	}
+	
+	
+}
